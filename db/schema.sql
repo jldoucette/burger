c3bba07a1123af112c4f301eb2f0f69830e6cb5d
jldoucette <jd@jonathandoucette.com>
2017-06-24 22:15:33 -0700

    updated schema for heroku db

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,13 @@ date DATETIME,
 PRIMARY KEY (id)
 );
 
+USE ewdx9c4tagr0c8b6;
+
+CREATE TABLE burgers(
+id INT  AUTO_INCREMENT NOT NULL,
+burger_name varchar(255) NOT NULL,
+devoured boolean,
+date DATETIME,
+createdAt TIMESTAMP NOT NULL,
+PRIMARY KEY(id)
+);
