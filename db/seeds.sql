807457893b4ee4138bafcd6e1e90c5095fee2dd0
jldoucette <jd@jonathandoucette.com>
2017-06-24 22:30:01 -0700

    Heroku fixes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
-INSERT INTO burgers(burger_name,devoured) VALUES ('Double Cheese with Egg', false);
-INSERT INTO burgers(burger_name,devoured) VALUES ('Plain Jane', false);
-INSERT INTO burgers(burger_name,devoured) VALUES ('California',false);
+INSERT INTO burgers(burger_name,devoured) VALUES ('Double Cheeseburger with Egg', false);
+INSERT INTO burgers(burger_name,devoured) VALUES ('Plain Jane Burger', false);
+INSERT INTO burgers(burger_name,devoured) VALUES ('California Burger',false);
