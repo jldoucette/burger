2d3a6c36fa374d1c9a6c73a0a798cb8146a878c7
jldoucette <jd@jonathandoucette.com>
2017-06-24 21:45:39 -0700

    Cleaned Up Code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
-INSERT INTO burgers(burger_name,devoured) VALUES ('Double with egg', false);
+INSERT INTO burgers(burger_name,devoured) VALUES ('Double Cheese with Egg', false);
 INSERT INTO burgers(burger_name,devoured) VALUES ('Plain Jane', false);
 INSERT INTO burgers(burger_name,devoured) VALUES ('California',false);
